7827fae6be72f5462d7b5642d3ae958c75307263
Shaun Dowling <shaunmdowling@gmail.com>
2012-12-02 18:23:35 +0000

    Removed unused files and changed the ,gitignore

--- logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-def make_logger(name):
-    logger = logging.getLogger('trader_AA')
-    hdlr = logging.FileHandler('trader_AA.log')
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    hdlr.setFormatter(formatter)
-    logger.addHandler(hdlr)
-    logger.setLevel(logging.DEBUG)
